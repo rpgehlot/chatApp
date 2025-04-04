4d5afe814391ed0bd5cef3ddfc718bc8e87ba4c4
Rituparn Gehlot <rpgehlot1991@gmail.com>
2025-04-04 11:46:08 +0530

    Added error on chat pager

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -2,7 +2,18 @@ import { createClient } from '@/utils/supabase/server';
 
 export default async function Instruments() {
   const supabase = await createClient();
-  const { data: messages } = await supabase.from("messages").select();
+  const { data: messages, error } = await supabase.from("messages").select();
 
-  return <pre>{JSON.stringify(messages, null, 2)}</pre>
+  return (
+    <div className='flex items-center align-items w-full flex-col p-4'>
+        <h1 className='text-2xl font-normal mb-5'>
+            Chat App
+        </h1>
+
+        {messages ? 
+            <pre>{JSON.stringify(messages, null, 2)}</pre> : 
+            <pre>{JSON.stringify(error, null, 2)}</pre>
+        }
+    </div>
+  );
 }
